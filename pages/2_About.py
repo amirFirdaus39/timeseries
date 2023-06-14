63390691ef1258abe361dd4d927322fa6beb73b2
Amir Firdaus Bin Abdul Hadi <80340482+amirFirdaus39@users.noreply.github.com>
2023-06-14 14:25:35 +0800

    Update 2_About.py

--- pages/2_About.py
+++ pages/2_About.py
@@ -4,37 +4,36 @@ import numpy as np
 import pandas as pd
 import altair as alt
 
-dfmw = pd.read_excel('data/Result.xlsx',sheet_name='MOS_WEEK')
-dftw = pd.read_excel('data/Result.xlsx',sheet_name='TOTAL_WEEK')
-dfrw = pd.read_excel('data/Result.xlsx',sheet_name='RATE_WEEK')
-dfmm = pd.read_excel('data/Result.xlsx',sheet_name='RATE_MONTH')
+dfmw = pd.read_excel('D:/Result.xlsx',sheet_name='MOS_WEEK')
+dftw = pd.read_excel('D:/Result.xlsx',sheet_name='TOTAL_WEEK')
+dfrw = pd.read_excel('D:/Result.xlsx',sheet_name='RATE_WEEK')
+dfmm = pd.read_excel('D:/Result.xlsx',sheet_name='RATE_MONTH')
 
-st.title("About - One Flow")
+st.title(":mag: About Project")
 tab1, tab2, tab3, tab4 = st.tabs(["Time Series", "Results","Dictionary","References"])
 
 with tab1:
     st.header("📝 Time Series Model")
-    st.divider()
-    st.subheader("What is time series?")
+    st.subheader(":interrobang: What is time series?")
     st.markdown("A time-series data is a series of data points or observations recorded at different or regular time intervals. In general, a time series is a sequence of data points taken at equally spaced time intervals. The frequency of recorded data points may be hourly, daily, weekly, monthly, quarterly or annually.")
     st.divider()
 
-    st.subheader("Components of a Time Series")
+    st.subheader(":ballot_box_with_check: Components of a Time Series")
     st.markdown("- **Trend** - The trend shows a general direction of the time series data over a long period of time. A trend can be increasing(upward), decreasing(downward), or horizontal(stationary).")
     st.markdown("- **Seasonality** - A seasonality is observed when there is a distinct repeated pattern observed between regular intervals due to seasonal factors. It could be because of the month of the year, the day of the month, weekdays or even time of the day.")
     st.divider()
 
-    st.subheader("Time Series Terminology")
+    st.subheader(":ballot_box_with_check: Time Series Terminology")
     st.markdown("- **Stationarity** - It shows the mean value of the series that remains constant over the time period. If past effects accumulate and the values increase towards infinity then stationarity is not met. A stationary series is one where the values of the series is not a function of time. So, the values are independent of time.")
     st.markdown("- **Differencing** - Differencing is used to make the series stationary and to control the auto-correlations. There may be some cases in time series analyses where we do not require differencing and over-differenced series can produce wrong estimates.")
     st.markdown("- **Exponential Smoothing** - Exponential smoothing in time series analysis predicts the one next period value based on the past and current value. It involves averaging of data such that the non-systematic components of each individual case or observation cancel out each other. The exponential smoothing method is used to predict the short term prediction.")
     st.divider()
 
-    st.subheader("Data Used")
+    st.subheader(":white_check_mark: Data Used")
     st.markdown("Data is taken from One Flow database/dashboard that consists data from 2022 until as of May 2023. There are 2 sets of data to be used for forecasting time series; data by week and data by month. Usually One Flow is interpreted by monthly analysis but due to lack of data points, the data is aggregrated by week to increase size of data. Though data by month is also used to see if smaller data can even forecast at the same accuracy or better than its by-week counterpart. If similar result acquired for both of this data, data by-month will be used due to more simpler process (predicting 'Rate' directly instead of predicting 'MOS Completed' and 'Total MOS' and then calculate the 'Rate' in by-week data) as an equivalent trade-off.")
     st.divider()
 
-    st.subheader("Model Used")
+    st.subheader(":computer: Model Used")
     st.markdown("**1) HWES - Holt-Winter’s Exponential Smoothing**")
     st.markdown("This model takes into account the trend and seasonality while doing the forecasting. This method has 3 major aspects for performing the predictions. It has an average value with the trend and seasonality.")
     st.markdown("- **Exponential Smoothing:** Simple exponential smoothing as the name suggest is used for forecasting when the data set has no trends or seasonality.")
@@ -54,7 +53,7 @@ with tab1:
     st.markdown("In the VAR model, each variable is modeled as a linear combination of past values of itself and the past values of other variables in the system. Since you have multiple time series that influence each other, it is modeled as a system of equations with one equation per variable (time series).")
     st.divider()
 
-    st.subheader("Tests Used")
+    st.subheader(":mag_right: Tests Used")
     st.markdown("**1) ADF Tests - Augmented Dickey-Fuller** [stationarity test]")
     st.markdown("In probability theory and statistics, a unit root is a feature of some stochastic processes (such as random walks) that can cause problems in statistical inference involving time series models. In simple terms, the unit root is non-stationary but does not always have a trend component.")
     st.markdown("- Stationary: p-value <= 0.05 & test statistic <= critical value")
@@ -78,7 +77,7 @@ with tab1:
 
 with tab2:
     st.header("📝 Tabulation of Results")
-    st.subheader("Evaluation Metrics")
+    st.subheader(":bar_chart: Evaluation Metrics")
     st.markdown("For this time series, we are using MAE (Mean Absolute Error), RMSE (Root Mean Square Error) and MAPE (Mean Absolute Percentage Error)")
     st.markdown("1) **MAE** - The MAE is defined as the average of the absolute difference between forecasted and true values. The lower the MAE value, the better the model; a value of zero indicates that the forecast is error-free. However, because MAE does not reveal the proportional scale of the error, it can be difficult to distinguish between large and little errors. MAE might obscure issues related to low data volume. ")
     st.markdown("2) **RMSE** - MSE is defined as the average of the error squares. It is also known as the metric that evaluates the quality of a forecasting model or predictor. MSE also takes into account variance (the difference between anticipated values) and bias (the distance of predicted value from its true value). The RMSE number is in the same unit as the projected value, which is an advantage of this technique. The wider the gap between RMSE and MAE, the more erratic the error size. This statistic can mask issues with low data volume.")
@@ -86,7 +85,7 @@ with tab2:
     st.markdown("Both MAE/MAPE and RMSE measures the magnitude of errors in a set of predictions. The major difference between MAE/MAPE and RMSE is the impact of the large errors. For example, if some prediction data points are large outliers errors when compared to the ground truth, those large errors will be diluted in the mean of MAE/MAPE while RMSE score will be higher because of the square operation.")
     st.divider()
 
-    st.subheader("Model Evaluations")
+    st.subheader(":white_check_mark: Model Evaluations")
     col1, col2 = st.columns(2)
     col1.write("Train/Test for MOS_COMPLETED by Week")
     col1.dataframe(dfmw,use_container_width=True)
@@ -99,7 +98,7 @@ with tab2:
     col2.dataframe(dfmm,use_container_width=True)
     st.divider()
 
-    st.subheader("Choosing Best Model")
+    #st.subheader(":white_check_mark: Choosing Best Model")
     st.markdown("")
     # chart = alt.Chart(dfmw).mark_line().encode(
     # x=alt.X('MAE'),
